9058e3a63fb9d73532606248a78dc3afd07a5c29
Silas Waller <silaswaller1@gmail.com>
2022-06-22 15:47:28 -0500

    inital commit

---
+++ fronts1.js
@@ -0,0 +1,43 @@
+class SLLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(value) {
+        var newNode = new SLLNode(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return this.head;
+    }
+
+    removeFront() {
+        if (this.head == null) {
+            return this.head;
+        }
+        var removedNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head;
+    }
+
+    front() {
+        if (this.head == null) {
+            return null;
+        } else {
+            return this.head.value;
+        }
+    }
+}
+
+var mySLL = new SLL();
+
+mySLL.addFront(10);
+mySLL.removeFront();
+console.log(mySLL.front());
